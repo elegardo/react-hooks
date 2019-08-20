6c93a73083f6611525bf2b3c469ba48e35e1bb1d
elegardo <elegardo@gmail.com>
2019-08-19 22:19:16 -0400

    crea defaultValue para el constexto

--- src/contexts/Counter/context.js
+++ src/contexts/Counter/context.js
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react';
 
-// Create a context which gives Provider and Consumer
-const CounterContext = createContext([0, () => {}]);
+const defaultValue = [0, () => {}]
+
+const CounterContext = createContext(defaultValue);
 
 export { CounterContext };
